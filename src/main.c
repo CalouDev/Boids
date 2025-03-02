01d9ddd6a50d4e77506795000f58aa059ccff08d
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-03-02 20:23:06 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,7 +7,6 @@
 
 #define WIN_W 1280
 #define WIN_H 640
-#define BOID_SPD 0.01
 
 static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
@@ -20,7 +19,7 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv) {
 	if (!SDL_Init(SDL_INIT_AUDIO)) return SDL_APP_FAILURE;
 	if (!TTF_Init()) return SDL_APP_FAILURE;
 
-    window = SDL_CreateWindow("Boïds simulation", WIN_W, WIN_H, SDL_WINDOW_OPENGL);
+    window = SDL_CreateWindow("Boïds", WIN_W, WIN_H, SDL_WINDOW_OPENGL);
     if (!window) return SDL_APP_FAILURE;
 
     renderer = SDL_CreateRenderer(window, NULL);
@@ -60,12 +59,18 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 	SDL_RenderClear(renderer);	
 
 	for (int i = 0; i < boidCount; ++i) {
-		//rotateBoid(&boids[i], 0.0003);
-		if (boids[i].dir >= 360) boids[i].dir = 0;
+		regulatedAngle(boids[i].dir);
+	
+		avoidBorder(&boids[i], 0, WIN_W, 0, WIN_H);
+
 		for (int j = 0; j < 3; ++j) {
-			boids[i].verticesPosition[j].position.x += BOID_SPD * cos(toRad(boids[i].dir));
-			boids[i].verticesPosition[j].position.y += BOID_SPD * sin(toRad(boids[i].dir));
+			boids[i].verticesPosition[j].position.x += BOID_SPD * SDL_cosf(toRad(boids[i].dir));
+			boids[i].verticesPosition[j].position.y += BOID_SPD * SDL_sinf(toRad(boids[i].dir));
 		}
+
+		boids[i].mid.x = (boids[i].verticesPosition[0].position.x + boids[i].verticesPosition[1].position.x + boids[i].verticesPosition[2].position.x) / 3.0;
+		boids[i].mid.y = (boids[i].verticesPosition[0].position.y + boids[i].verticesPosition[1].position.y + boids[i].verticesPosition[2].position.y) / 3.0;
+
 		SDL_RenderGeometry(renderer, NULL, boids[i].verticesPosition, 3, NULL, 3);
 	}
 
@@ -73,3 +78,4 @@ SDL_AppResult SDL_AppIterate(void *appstate) {
 
 	return SDL_APP_CONTINUE;
 }
+
