d5a9d74f7eeed4ffbff28508d82d4cfa5e00db54
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-03-02 20:23:49 +0100

    Update boid.h

--- include/boid.h
+++ include/boid.h
@@ -1,19 +1,22 @@
 #ifndef BOID
 #define BOID
 
-#define BOID_W 10
-#define BOID_H 20
-
 #include <math.h>
 #include <SDL3/SDL.h>
+
+#define BOID_W 10
+#define BOID_H 20
+#define BOID_SPD 0.09
+#define AVOID_RANGE 4
 
 typedef struct {
 	SDL_Vertex verticesPosition[3];
 	float dir;
-	SDL_Point mid;
+	SDL_FPoint mid;
 } Boid;
 
 void createBoid(Boid **boids, short *boidCount, float x, float y);
 void rotateBoid(Boid *boid, float angle);
+void avoidBorder(Boid *boid, float x1, float x2, float y1, float y2);
 
 #endif
