a3d1a86d643a197db27762f8d915ea41834cf1ff
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-03-02 20:24:14 +0100

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -4,5 +4,8 @@
 #include <SDL3/SDL.h>
 
 float toRad(float degree);
+float regulatedAngle(float degree);
+float pointsDist(float x1, float y1, float x2, float y2);
+SDL_FPoint angleToVec(float angle);
 
 #endif
