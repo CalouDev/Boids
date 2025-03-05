4b9ee5ad31c34b3baedf3b6b882d903822d75e06
CalouDev <71971165+CalouDev@users.noreply.github.com>
2025-03-05 20:55:41 +0100

    Update utils.h

--- include/utils.h
+++ include/utils.h
@@ -6,6 +6,6 @@
 float toRad(float degree);
 float regulatedAngle(float degree);
 float pointsDist(float x1, float y1, float x2, float y2);
-SDL_FPoint angleToVec(float angle);
+void drawCircle(SDL_Renderer *renderer, float centerX, float centerY, float radius);
 
 #endif
